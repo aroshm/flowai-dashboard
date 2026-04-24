6a9a9caf98c2bb2bbe06fc232eeca633d7180356
Arosh Athukorala <aroshmathukorala@gmail.com>
2026-04-24 07:28:31 +0530

    added menu icons and menu collapse feature

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="flex h-screen">
-      <Sidebar />
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
 
       <div className="flex-1 flex flex-col">
         <Header />
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,15 +1,62 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../../assets/flow-ai-logo.png";
+import CollapseLogo from "../../assets/flow-ai-logo-collapsed.png";
+import { MdMenu, MdDashboard, MdPeople, MdSettings } from "react-icons/md";
 
-const Sidebar = () => {
+type SidebarProps = {
+  collapsed: boolean;
+  setCollapsed: (val: boolean) => void;
+};
+
+const Sidebar = ({ collapsed, setCollapsed }: SidebarProps) => {
   return (
-    <div className="w-64 bg-white border-r p-6">
-      <img src={Logo} alt="Flow AI logo" width="150px" />
+    <div
+      className={`${collapsed ? "w-20" : "w-64"} transition bg-white border-r p-6`}
+    >
+      <div className="flex justify-end">
+        <MdMenu
+          className="h-7 w-7 mb-2.5"
+          onClick={() => setCollapsed(!collapsed)}
+        />
+      </div>
+
+      <img
+        src={collapsed ? CollapseLogo : Logo}
+        alt="Flow AI logo"
+        width="150px"
+      />
 
       <nav className="flex flex-col gap-4 pt-5">
-        <Link to="/">Dashboard</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/settings">Settings</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `${!collapsed ? "px-3 " : ""} py-2 rounded-lg flex items-center gap-1.5 
+          ${isActive && !collapsed ? "bg-indigo-100 text-indigo-600" : isActive && collapsed ? "text-indigo-600" : "text-gray-600"}`
+          }
+        >
+          <MdDashboard className="h-5 w-5" />
+          {!collapsed && <span>Dashboard</span>}
+        </NavLink>
+        <NavLink
+          to="/users"
+          className={({ isActive }) =>
+            `${!collapsed ? "px-3 " : ""} py-2 rounded-lg flex items-center gap-1.5 
+          ${isActive && !collapsed ? "bg-indigo-100 text-indigo-600" : isActive && collapsed ? "text-indigo-600" : "text-gray-600"}`
+          }
+        >
+          <MdPeople className="h-5 w-5" />
+          {!collapsed && <span>Users</span>}
+        </NavLink>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) =>
+            `${!collapsed ? "px-3 " : ""} py-2 rounded-lg flex items-center gap-1.5 
+          ${isActive && !collapsed ? "bg-indigo-100 text-indigo-600" : isActive && collapsed ? "text-indigo-600" : "text-gray-600"}`
+          }
+        >
+          <MdSettings className="h-5 w-5" />
+          {!collapsed && <span>Settings</span>}
+        </NavLink>
       </nav>
     </div>
   );
