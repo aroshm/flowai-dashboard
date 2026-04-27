85964355022360a941a427898fbff4638d202a19
Arosh Athukorala <34985891+aroshm@users.noreply.github.com>
2026-04-27 13:05:33 +0530

    Revert "Version2" (#2)

--- src/components/table/useHeader.tsx
+++ src/components/table/useHeader.tsx
@@ -1,1 +1,15 @@
-export { default } from "../../hooks/usePageTitle";
+import { useEffect } from "react";
+
+const useHeader = (header?: string) => {
+  useEffect(() => {
+    const pageHeader = document.getElementById("page-header");
+
+    if (!pageHeader || !header) return;
+
+    pageHeader.textContent = header;
+  }, [header]);
+
+  return null;
+};
+
+export default useHeader;
